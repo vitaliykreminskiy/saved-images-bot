6920f84e71e98698ab992dc8a2712d73418ff02f
vitaliykreminskiy <vkrmk13@gmail.com>
2020-03-21 16:45:15 +0200

    Added reply with saved pictures count

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const dotenv = require('dotenv').config();
+const request = require('request');
 const Telegraf = require('telegraf');
 const Keyboard = require('telegraf-keyboard');
 const LocalSession = require('telegraf-session-local')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('oldschool', (ctx) => ctx.reply('Hello'));
-bot.command('modern', (ctx) => ctx.reply('Yo'));
-bot.command('hipster', (ctx) => ctx.reply('λ'));
+bot.command('random', (ctx) => {
+    const endpoint = 'http://api.vk.com/photos.get?access_token=' + process.env.VK_TOKEN + '&album_id=saved&v=5.103';
+    request (
+        {
+            method: 'GET',
+            uri: endpoint
+        }, (error, response, body) => {
+            let decodedResponse = JSON.parse(body);
+            ctx.reply(decodedResponse.response.count);
+        }
+    )
+});
 
 bot.startPolling();

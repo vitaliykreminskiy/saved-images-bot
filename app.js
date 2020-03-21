881a9dece5c0935c1df111d6708afe858f84c8a9
vitaliykreminskiy <vkrmk13@gmail.com>
2020-03-21 16:50:54 +0200

    Fixed endpoint

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const LocalSession = require('telegraf-session-local')
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('random', (ctx) => {
-    const endpoint = 'http://api.vk.com/photos.get?access_token=' + process.env.VK_TOKEN + '&album_id=saved&v=5.103';
+    const endpoint = 'http://api.vk.com/method/photos.get?access_token=' + process.env.VK_TOKEN + '&album_id=saved&v=5.103';
     request (
         {
             method: 'GET',
             uri: endpoint
         }, (error, response, body) => {
             let decodedResponse = JSON.parse(body);
-            ctx.reply(body);
+            ctx.reply(decodedResponse.response.count);
         }
     )
 });
